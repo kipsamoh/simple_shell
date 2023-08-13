d850ccdac439cba2245bcbf3ba8c1241f418272e
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-08-13 12:33:42 +0300

    execute_command.c

--- execute_command.c
+++ execute_command.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * is_cdir - checks ":" if is in the current directory.
- * @path: type char pointer char.
- * @i: type int pointer of index.
- * Return: 1 if the path is searchable in the cd, 0 otherwise.
+ * is_cdir - checks ":" wether is current directory.
+ * @path: type char ptr char.
+ * @i: type int ptr of index.
+ * Return: 1 if the path is searchable in the cd, else 0.
  */
 int is_cdir(char *path, int *i)
 {
@@ -23,16 +23,16 @@ int is_cdir(char *path, int *i)
 }
 
 /**
- * _which - locates a command
+ * _which - finds a cmd
  *
- * @cmd: command name
+ * @cmd: name of the command
  * @_environ: environment variable
- * Return: location of the command.
+ * Return:  command location.
  */
 char *_which(char *cmd, char **_environ)
 {
 	char *path, *ptr_path, *token_path, *dir;
-	int len_dir, len_cmd, i;
+	int len_dir, len_cmd, x;
 	struct stat st;
 
 	path = _getenv("PATH", _environ);
@@ -41,10 +41,10 @@ char *_which(char *cmd, char **_environ)
 		ptr_path = _strdup(path);
 		len_cmd = _strlen(cmd);
 		token_path = _strtok(ptr_path, ":");
-		i = 0;
+		x = 0;
 		while (token_path != NULL)
 		{
-			if (is_cdir(path, &i))
+			if (is_cdir(path, &x))
 				if (stat(cmd, &st) == 0)
 					return (cmd);
 			len_dir = _strlen(token_path);
@@ -73,52 +73,52 @@ char *_which(char *cmd, char **_environ)
 }
 
 /**
- * is_executable - determines if is an executable
+ * is_executable - checks if file is executable
  *
  * @datash: data structure
- * Return: 0 if is not an executable, other number if it does
+ * Return: 0 if executable, else other if executable
  */
 int is_executable(data_shell *datash)
 {
 	struct stat st;
-	int i;
-	char *input;
+	int x;
+	char *in_put;
 
-	input = datash->args[0];
-	for (i = 0; input[i]; i++)
+	in_put = datash->args[0];
+	for (x = 0; in_put[x]; x++)
 	{
-		if (input[i] == '.')
+		if (in_put[x] == '.')
 		{
-			if (input[i + 1] == '.')
+			if (in_put[x + 1] == '.')
 				return (0);
-			if (input[i + 1] == '/')
+			if (in_put[x + 1] == '/')
 				continue;
 			else
 				break;
 		}
-		else if (input[i] == '/' && i != 0)
+		else if (in_put[x] == '/' && x != 0)
 		{
-			if (input[i + 1] == '.')
+			if (in_put[x + 1] == '.')
 				continue;
-			i++;
+			x++;
 			break;
 		}
 		else
 			break;
 	}
-	if (i == 0)
+	if (x == 0)
 		return (0);
 
-	if (stat(input + i, &st) == 0)
+	if (stat(in_put + x, &st) == 0)
 	{
-		return (i);
+		return (x);
 	}
 	get_error(datash, 127);
 	return (-1);
 }
 
 /**
- * check_error_cmd - verifies if user has permissions to access
+ * check_error_cmd - checks for user access permissions
  *
  * @dir: destination directory
  * @datash: data structure
@@ -155,9 +155,9 @@ int check_error_cmd(char *dir, data_shell *datash)
 }
 
 /**
- * cmd_exec - executes command lines
+ * cmd_exec - command lines execution
  *
- * @datash: data relevant (args and input)
+ * @datash: relevant data (args and input)
  * Return: 1 on success.
  */
 int cmd_exec(data_shell *datash)
