d4cdc064122b044a10a86e5559838c389c9fd469
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-08-13 08:28:21 +0300

    check_builtins.c

--- check_builtins.c
+++ check_builtins.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * exec_line - finds builtins and commands
+ * exec_line - finds built-ins and commands
  *
- * @datash: data relevant (args)
+ * @datash: relevant data (args)
  * Return: 1 on success.
  */
 int exec_line(data_shell *datash)
