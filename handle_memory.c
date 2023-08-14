d7307df64730fcfe5b042294b974586c2e649ce6
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-08-14 14:06:06 +0300

    handle_memory.c

--- handle_memory.c
+++ handle_memory.c
@@ -1,31 +1,31 @@
 #include "main.h"
 
 /**
- * _memcpy - copies information between void pointers.
+ * _memcpy - copies info_rmation between void pointers.
  * @newptr: destination pointer.
  * @ptr: source pointer.
  * @size: size of the new pointer.
  *
- * Return: no return.
+ * Return: void.
  */
 void _memcpy(void *newptr, const void *ptr, unsigned int size)
 {
 	char *char_ptr = (char *)ptr;
 	char *char_newptr = (char *)newptr;
-	unsigned int i;
+	unsigned int x;
 
-	for (i = 0; i < size; i++)
-		char_newptr[i] = char_ptr[i];
+	for (x = 0; x < size; x++)
+		char_newptr[x] = char_ptr[x];
 }
 
 /**
  * _realloc - reallocates a memory block.
- * @ptr: pointer to the memory previously allocated.
+ * @ptr: pointer to the mem_ory previously allocated.
  * @old_size: size, in bytes, of the allocated space of ptr.
  * @new_size: new size, in bytes, of the new memory block.
  *
- * Return: ptr.
- * if new_size == old_size, returns ptr without changes.
+ * Return: poiter.
+ * if new_size == old_size, returns pointer without changes.
  * if malloc fails, returns NULL.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
@@ -70,7 +70,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 char **_reallocdp(char **ptr, unsigned int old_size, unsigned int new_size)
 {
 	char **newptr;
-	unsigned int i;
+	unsigned int x;
 
 	if (ptr == NULL)
 		return (malloc(sizeof(char *) * new_size));
@@ -82,8 +82,8 @@ char **_reallocdp(char **ptr, unsigned int old_size, unsigned int new_size)
 	if (newptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < old_size; i++)
-		newptr[i] = ptr[i];
+	for (x = 0; x < old_size; x++)
+		newptr[x] = ptr[x];
 
 	free(ptr);
 
